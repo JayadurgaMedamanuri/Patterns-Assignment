eef8192189d5b1a1ec944acea72ebe486c6be673
JayadurgaMedamanuri <115568260+JayadurgaMedamanuri@users.noreply.github.com>
2022-10-27 17:51:09 +0530

    Pattern3 Code

---
+++ Pattern3.java
@@ -0,0 +1,28 @@
+package com.loops.assignement;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		int n=19;
+		for(int i=0;i<n;i++) {
+			
+			for (int j=0;j<n;j++) {
+				if((j==0 && i>(n-1)/2)|| (j==(n-1) && i>(n-1)/2) ||
+						i+j >= (n-1) + (n-1)/2 || i-j>=(n-1)/2)
+						
+						
+				{
+				System.out.print("*");
+				}
+				else
+				{
+				System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+		}
+
+	}
+
+}
